8db9f2fced7a9b0d6127b7ecbbf68d00f354bbd3
matekuna92 <kunamate66@gmail.com>
2023-04-08 20:39:02 +0200

    mainNav styles

---
+++ components/layout/MainNavigation.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+import styles from './MainNavigation.module.css';
+
+
+const MainNavigation = () => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.mainLogo}> React Meetups </div>
+            <ul>
+                <li><Link href='/all-meetups'> All Meetups </Link></li>
+                <li><Link href='/new-meetup'> Add New Meetup </Link></li>
+            </ul>
+        </header>
+    )
+}
+
+export default MainNavigation;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import MainNavigation from "../components/layout/MainNavigation";
+
+
+const HomePage = () => {
+    return (
+        <>
+            <MainNavigation />
+            <h1> Home Page </h1>
+        </>
+    )
+}
+
+export default HomePage;
