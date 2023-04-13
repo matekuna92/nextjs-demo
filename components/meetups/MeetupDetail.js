509c6cfa24ce37b683547623961dab4822f54d84
MATEKUNA <kuna.mate@parlament.hu>
2023-04-13 11:24:43 +0200

    useEffect - MeetupList is empty array on first render before useEffect runs problem

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -3,13 +3,12 @@ import {Fragment} from 'react';
 import styles from './MeetupDetail.module.css';
 
 const MeetupDetail = (props) => {
-    return <Fragment>
-        <img src={props.image}
-                    alt={props.title} />
-                <h1>{props.title}</h1>
-                <address>{props.address}</address>
-                <p>{props.desc}</p>
-        </Fragment>
+    return <section className={styles.detail}>
+        <img src={props.image} alt={props.title} className={styles.img} />
+        <h1>{props.title}</h1>
+        <address>{props.address}</address>
+        <p>{props.desc}</p>
+    </section>
 }
 
 export default MeetupDetail;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,42 @@
+import { useEffect, useState } from "react";
 import MainNavigation from "../components/layout/MainNavigation";
 import MeetupList from "../components/meetups/MeetupList";
 
+const TEST_MEETUPLIST = [
+    {
+        id: '1',
+        title: 'Title 1',
+        image: 'https://images.unsplash.com/photo-1576085898323-218337e3e43c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+        address: '1111 Test Street 1',
+        desc: 'First meetup'
+    },
+    {
+        id: '2',
+        title: 'Title 2',
+        image: 'https://images.unsplash.com/photo-1568992688065-536aad8a12f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
+        address: '2222 Test Street 2',
+        desc: 'Second meetup'
+    },
+    {
+        id: '3',
+        title: 'Title 3',
+        image: 'https://images.unsplash.com/photo-1524178232363-1fb2b075b655?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+        address: '3333 Test Street 3',
+        desc: 'Third meetup'
+    }
+];
+
 
 const HomePage = () => {
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    useEffect(() => {
+        setLoadedMeetups(TEST_MEETUPLIST);
+    }, []);
+
     return (
         <>
-            <MeetupList />
+            <MeetupList meetups={loadedMeetups} />
         </>
     )
 }
