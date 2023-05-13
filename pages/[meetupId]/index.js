660a30f66b6fc326bf0a217a591d5c44e2ec5aa4
matekuna92 <kunamate66@gmail.com>
2023-05-13 20:42:34 +0200

    [meetupId] dynamic meetup data in getStaticProps, getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,46 +1,61 @@
 
+import { MongoClient, ObjectId } from 'mongodb';
+
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 const MeetupDetails = (props) => {
     return <MeetupDetail
-        image='https://images.unsplash.com/photo-1576085898323-218337e3e43c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-        title='First Meetup'
-        address='Some Street 15, Some City'
-        desc='Meetup 1 Description'
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        desc={props.meetupData.desc}
     />
 }
 
 // context also exists in getStaticProps, but request and response cant be accessed like in getServerSideProps
 export const getStaticProps = async (context) => {
-    const params = context.params;
+    const meetupId = context.params.meetupId;
+
+    const client = await MongoClient.connect('mongodb+srv://jsnext:js123123@nextjsdemo.olsk22u.mongodb.net/test');
+    const db = client.db();
+    
+    const meetupsCollection = db.collection('meetups');
+    // return all documents, only extracting the id field values from every item
+    const selectedMeetup = await meetupsCollection.findOne({ _id: new ObjectId(meetupId) });
+    console.log("selectedMeetup: ", selectedMeetup);
+
+    await client.close();
 
     return {
         props: {
             meetupData: {
-                id: params.meetupId,        // [meetupId] can be accessed from the URL
-                image: 'https://images.unsplash.com/photo-1576085898323-218337e3e43c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-                title: 'First Meetup',
-                address: 'Some Street 15, Some City',
-                desc: 'Meetup 1 Description'
+                id: selectedMeetup._id.toString(),
+                image: selectedMeetup.image,
+                title: selectedMeetup.title,
+                address: selectedMeetup.address,
+                desc: selectedMeetup.desc
             }
         }
     }
 }
 
 export const getStaticPaths = async () => {
+    const client = await MongoClient.connect('mongodb+srv://jsnext:js123123@nextjsdemo.olsk22u.mongodb.net/test');
+    const db = client.db();
+    
+    const meetupsCollection = db.collection('meetups');
+    // return all documents, only extracting the id field values from every item
+    const allMeetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+    await client.close();
+
     return {
-        paths: [
-            {
+        paths: 
+            allMeetups.map(meetup => ({
                 params: {
-                    meetupId: '1'
-                },
-            },
-            {
-                params: {
-                    meetupId: '2'
-                },
-            }
-        ],
+                    meetupId: meetup._id.toString()
+                }
+            })),
         // if false: it means that paths array contains ALL supported parameter values for meetupID-s,
         // if user enters a path which isnt defined in the array, user will get a 404 error
         // if true: it contains only some of them, needs fallback. NextJS tries to generate a page for the incoming request
